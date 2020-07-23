4757990916e4d62fd83144de666b5126bfd326cc
kelsey <krmmaine@gmail.com>
2020-07-23 16:20:32 -0600

    don't need code to profile anymore with built in profiling in pro version of IDE

--- main.py
+++ main.py
@@ -53,20 +53,4 @@ def main():
 
     return
 
-
-'''
-import cProfile
-import re
-cProfile.run('re.compile("foo|bar")', 'restats')
-
-import pstats
-p = pstats.Stats('restats')
-p.strip_dirs().sort_stats(-1).print_stats()
-
-p.sort_stats('name')
-p.print_stats()
-
-print("profiling done")
-'''
-
 main()
