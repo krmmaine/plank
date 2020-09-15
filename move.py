61fda65e58ad6e8593a2df33eedbc37d552312d0
kelsey <krmmaine@gmail.com>
2020-09-15 13:58:20 -0600

    printing out to a file when the cell backtracks on itself

--- move.py
+++ move.py
@@ -1,36 +1,81 @@
-def move(cell, time, stay, left, right, down, random, yPos, xPos, occupied, fib, vegf, pro, movedUp, movedDown, movedLeft, movedRight):
+def move(cell, time, stay, left, right, down, random, yPos, xPos, occupied, fib, vegf, pro, movement):
+
+    file = open("tracking_backtracks.txt", "w")
 
     # UP
     if random > stay + left + right + down:
         yPos[cell][time+1] = yPos[cell][time] + 1
-        print("moved up")
-        movedUp += 1
+        if yPos[cell][time] > 0:
+            movement.append(0)
+            if len(movement) > 1:
+                if movement[len(movement)-2] == 1:
+                    print("backtrack")
+                    file.write("BACKTRACK: time = " + str(time))
+                    file.write("moved up")
+                    file.write("fib: " + str(fib[yPos[cell][time+1]][xPos[cell][time]]))
+                    file.write("vegf: " + str(vegf[yPos[cell][time+1]][xPos[cell][time]]))
+                    file.write("pro: " + str(pro[yPos[cell][time+1]][xPos[cell][time]]))
+                    file.write("prob up = " + str(1-left-right-down-stay))
+                    file.write("prob down = " + str(down))
+                    file.write("prob right = " + str(right))
+                    file.write("prob left = " + str(left))
+
     # DOWN
     elif random > stay + left + right:
         yPos[cell][time+1] = yPos[cell][time] - 1
-        print("moved down")
-        movedDown += 1
+        if yPos[cell][time] > 0:
+            movement.append(1)
+            if movement[len(movement) - 2] == 0:
+                print("backtrack")
+                file.write("BACKTRACK: time = " + str(time))
+                file.write("moved down")
+                file.write("fib: " + str(fib[yPos[cell][time + 1]][xPos[cell][time]]))
+                file.write("vegf: " + str(vegf[yPos[cell][time + 1]][xPos[cell][time]]))
+                file.write("pro: " + str(pro[yPos[cell][time + 1]][xPos[cell][time]]))
+                file.write("prob up = " + str(1 - left - right - down - stay))
+                file.write("prob down = " + str(down))
+                file.write("prob right = " + str(right))
+                file.write("prob left = " + str(left))
+
     # RIGHT
     elif random > stay + left:
         xPos[cell][time+1] = xPos[cell][time] + 1
-        print("moved right")
-        movedRight += 1
+        if yPos[cell][time] > 0:
+            movement.append(2)
+            if movement[len(movement) - 2] == 3:
+                print("backtrack")
+                file.write("BACKTRACK: time = " + str(time))
+                file.write("moved right")
+                file.write("fib: " + str(fib[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("vegf: " + str(vegf[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("pro: " + str(pro[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("prob up = " + str(1 - left - right - down - stay))
+                file.write("prob down = " + str(down))
+                file.write("prob right = " + str(right))
+                file.write("prob left = " + str(left))
+
     # LEFT
     elif random > stay:
         xPos[cell][time+1] = xPos[cell][time] - 1
-        print("moved left")
-        movedLeft += 1
+        if yPos[cell][time] > 0:
+            movement.append(3)
+            if movement[len(movement) - 2] == 2:
+                print("backtrack")
+                file.write("BACKTRACK: time = " + str(time))
+                file.write("moved left")
+                file.write("fib: " + str(fib[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("vegf: " + str(vegf[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("pro: " + str(pro[yPos[cell][time]][xPos[cell][time + 1]]))
+                file.write("prob up = " + str(1 - left - right - down - stay))
+                file.write("prob down = " + str(down))
+                file.write("prob right = " + str(right))
+                file.write("prob left = " + str(left))
+
     # update occupancy based on cell movement
     if random > stay:
         occupied[yPos[cell][time]][xPos[cell][time]] -= 1
         occupied[yPos[cell][time+1]][xPos[cell][time+1]] += 1
-    if time % 1 == 0:
-        print("fib: " + str(fib[yPos[cell][time-1]][xPos[cell][time-1]]))
-        print("vegf: " + str(vegf[yPos[cell][time-1]][xPos[cell][time-1]]))
-        print("pro: " + str(pro[yPos[cell][time-1]][xPos[cell][time-1]]))
-        print("moves up: " + str(movedUp))
-        print("moves down: = " + str(movedDown))
-        print("moves right: = " + str(movedRight))
-        print("moves left: " + str(movedLeft))
 
-    return movedUp, movedDown, movedLeft, movedRight
+    file.close()
+
+    return movement
